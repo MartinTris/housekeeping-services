8685a1d4fe43bc7eb892e86aaeeb402fb2687666
Martin Tristan <tmo1920@dlsud.edu.ph>
2025-11-27 01:23:06 +0800

    m

--- client/src/pages/guest/BorrowItems.js
+++ client/src/pages/guest/BorrowItems.js
@@ -35,7 +35,7 @@ const BorrowItems = () => {
       openTime = "8:00 AM";
       closeTime = "5:00 PM";
     } else if (facility === "Hotel Rafael") {
-      openMinutes = 6 * 60;
+      openMinutes = 1 * 60;
       closeMinutes = 22 * 60; 
       openTime = "6:00 AM";
       closeTime = "6:00 PM";
